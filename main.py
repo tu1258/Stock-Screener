ca0b5784b46098391a181307f0b5b3cdee30dca6
tu1258 <59950011+tu1258@users.noreply.github.com>
2026-02-01 17:01:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("RS screener is alive")
+import yfinance as yf
+
+spx = yf.download("^GSPC", period="5d", interval="1d")
+last_close = spx["Close"].iloc[-1]
+
+print("SP500 last close:", float(last_close))
