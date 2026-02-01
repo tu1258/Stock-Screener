e80d7d10a1cb580dcb3fedc0e582173d38a2d0fa
tu1258 <59950011+tu1258@users.noreply.github.com>
2026-02-01 17:05:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import yfinance as yf
 
 spx = yf.download("^GSPC", period="5d", interval="1d")
+
 last_close = spx["Close"].iloc[-1]
+
+# 如果是 Series，就取第一個值
+if hasattr(last_close, "iloc"):
+    last_close = last_close.iloc[0]
 
 print("SP500 last close:", float(last_close))
