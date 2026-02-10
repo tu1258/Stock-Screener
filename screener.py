e4c4db94f0ede7c61c1e2d8b51aa471d626aa701
tu1258 <59950011+tu1258@users.noreply.github.com>
2026-02-10 12:56:16 +0800

    Update screener.py

--- screener.py
+++ screener.py
@@ -27,8 +27,8 @@ def compute_indicators_vectorized(df):
 
     df['atr_5'] = df.groupby('ticker')['tr'].transform(lambda x: x.rolling(5).mean())
     df['atr_10'] = df.groupby('ticker')['tr'].transform(lambda x: x.rolling(10).mean())
-    df['atr_20'] = df.groupby('ticker')['tr'].transform(lambda x: x.rolling(20).mean())
-    df["atr_10_pct"] = df["atr_10"] / df["close"] * 100
+    df['atr_50'] = df.groupby('ticker')['tr'].transform(lambda x: x.rolling(50).mean())
+    df["atr_14_pct"] = df.groupby('ticker')['tr'].transform(lambda x: x.rolling(14).mean()) / df["close"] * 100
 
     # 均線
     df["ma20"] = df.groupby("ticker")["close"].transform(lambda x: x.rolling(20).mean())
@@ -76,12 +76,11 @@ def main():
     # ---------- 3. 技術分析篩選 ----------
     tech_filtered = latest_df[
         (latest_df["avg_value_10"] > 100) &
-        (latest_df["atr_10_pct"] > 1) & (latest_df["atr_10_pct"] < 10) &
+        (latest_df["atr_14_pct"] > 1) & (latest_df["atr_14_pct"] < 10) &
         (latest_df["close"] > latest_df["ma50"]) &
         (latest_df["ma50"] > latest_df["ma200"]) &
         (latest_df["up_vol_10"] > latest_df["down_vol_10"]) & 
         (latest_df["up_vol_5"] > latest_df["down_vol_5"]) 
-        #(latest_df['atr_square_10'] > latest_df['atr_square_5'])
         #(latest_df["range_5"] < latest_df["atr_5"] * 2.5)
         #(latest_df["range_10"] < latest_df["atr_10"] * 2.5)
     ]
@@ -92,7 +91,7 @@ def main():
         .sort_values("RS", ascending=False)[[
             "ticker", "RS", "close", "volume",
             "ma20", "ma50", "ma200",
-            "atr_5", "atr_10", "atr_square_5", "atr_square_10", "range_5", "range_10",
+            "atr_5", "atr_10", "atr_50", "range_5", "range_10",
             "up_vol_5", "down_vol_5", "up_vol_10", "down_vol_10", "avg_value_10"
         ]]
     )
