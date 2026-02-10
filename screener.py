4b6c9c66b591a6b1494db9d7e7c4e0d8f4146bc9
tu1258 <59950011+tu1258@users.noreply.github.com>
2026-02-10 14:13:56 +0800

    Update screener.py

--- screener.py
+++ screener.py
@@ -24,11 +24,12 @@ def compute_indicators_vectorized(df):
         (df['high'] - df['prev_close']).abs(),
         (df['low'] - df['prev_close']).abs()
     ], axis=1).max(axis=1)
-
+    df["tr_pct"] = df["tr"] / df["close"] * 100
+    
     df['atr_5'] = df.groupby('ticker')['tr'].transform(lambda x: x.rolling(5).mean())
     df['atr_10'] = df.groupby('ticker')['tr'].transform(lambda x: x.rolling(10).mean())
     df['atr_50'] = df.groupby('ticker')['tr'].transform(lambda x: x.rolling(50).mean())
-    df["atr_14_pct"] = df.groupby('ticker')['tr'].transform(lambda x: x.rolling(14).mean()) / df["close"] * 100
+    df["atr_14_pct"] = df.groupby('ticker')['tr_pct'].transform(lambda x: x.rolling(14).mean())
 
     # 均線
     df["ma20"] = df.groupby("ticker")["close"].transform(lambda x: x.rolling(20).mean())
