b8d6f17df87e2bdca8646e3a92ed42698ba3bc0c
tu1258 <59950011+tu1258@users.noreply.github.com>
2026-04-20 03:10:41 +0800

    Update hot_themes.py

--- hot_themes.py
+++ hot_themes.py
@@ -20,8 +20,8 @@ MIN_AVG_VALUE_10D = 100
 
 #GEMINI_MODEL_SUMMARY = "gemini-3.1-flash-lite-preview"
 #GEMINI_MODEL_THEMES  = "gemini-3-flash-preview"
-GEMINI_MODEL_SUMMARY = "gemini-2.5-pro"
-GEMINI_MODEL_THEMES  = "gemini-2.5-pro"
+GEMINI_MODEL_SUMMARY = "gemini-2.5-flash"
+GEMINI_MODEL_THEMES  = "gemini-2.5-flash"
 
 TODAY = datetime.date.today().strftime("%Y-%m-%d")
 
