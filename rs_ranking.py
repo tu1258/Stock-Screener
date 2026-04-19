4264d5258dc7da12c9e30124ddf3a317ffda0c98
tu1258 <59950011+tu1258@users.noreply.github.com>
2026-04-20 05:07:19 +0800

    Update rs_ranking.py

--- rs_ranking.py
+++ rs_ranking.py
@@ -31,7 +31,7 @@ def strength(closes):
         m10 = geo_monthly_return(closes, 10)
         m11 = geo_monthly_return(closes, 11)
         m12 = geo_monthly_return(closes, 12)
-        return m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + m10 + m11 + m12
+        return (m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9 + m10 + m11 + m12) / 12
     except Exception:
         return 0
 
