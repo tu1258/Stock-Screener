1bbea3fe935e83b19aafb0bd4fb662ebe2415e2f
tu1258 <59950011+tu1258@users.noreply.github.com>
2026-04-27 04:38:30 +0800

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -67,7 +67,7 @@ def load_rs95_liquid(rs_csv, stock_data_csv):
     atr_ok_set   = set(latest[latest["atr_14_pct"] > MIN_ATR_PCT].index)
     no_spike_set = set(latest[latest["tr_pct_max_14"] <= 25 * latest["atr_pct_13_excl"]].index)
 
-    valid_set = liquid_set & no_spike_set #& atr_ok_set 
+    valid_set = liquid_set & no_spike_set & atr_ok_set 
     debug = latest[latest.index.isin(valid_set)][["atr_14_pct", "tr_pct_max_14", "atr_pct_13_excl"]].copy()
     print(debug.to_string())
     return sorted([(t, rs) for t, rs in rs_map.items() if t in valid_set], key=lambda x: -x[1])
